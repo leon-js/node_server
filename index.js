365316f2dff8cd435ce7accfabed9d6c4577c835
lianglihao <807478609@qq.com>
2018-10-03 23:40:01 +0800

    测试代码

---
+++ index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.get("/",(req,res) => {
+  // res.write(`<link href="favicon.ico" rel="shortcut icon"></link>`)
+  res.send("INDES");
+})
+
+app.get('/api/getArticle', (req, res, next) => {
+  res.json({
+      data: '后台返回结果 getArticle'
+    });
+
+})
+
+// 监听端口
+const port = 5000;
+
+app.listen(port,() => {
+    console.log(`Server started on ${port}`);
+})
