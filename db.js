dc5626c874c599d5ab9dde226cd94dcc39080991
lianglihao <807478609@qq.com>
2018-10-04 15:05:29 +0800

    数据库链接测试（success）

---
+++ db.js
@@ -0,0 +1,12 @@
+
+
+module.exports = {
+    mysql:{
+        host:'localhost',
+        user:'root',
+        password:'12345678',
+        database:'myblog',
+        port:'3306'
+    }
+}
+
--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const path = require('path');
+const routerApi = require('./router');
+const bodyParser = require('body-parser'); // post 数据是需要
 const express = require('express');
 const app = express();
 
-app.get("/",(req,res) => {
-  // res.write(`<link href="favicon.ico" rel="shortcut icon"></link>`)
-  res.send("INDES");
-})
+app.use(bodyParser.json());
+// 后端api路由
+app.use('/api', routerApi);
+// app.get("/",(req,res) => {
+//   // res.write(`<link href="favicon.ico" rel="shortcut icon"></link>`)
+//   res.send("INDES");
+// })
 
-app.get('/api/getArticle', (req, res, next) => {
-  res.json({
-      data: '后台返回结果 getArticle'
-    });
+// app.get('/api/getArticle', (req, res, next) => {
+//   res.json({
+//       data: '后台返回结果 getArticle'
+//     });
 
-})
+// })
 
 // 监听端口
 const port = 5000;
