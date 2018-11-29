a1d6cab71ce618503445f7ba02c4ae4855bfa557
lianglihao <807478609@qq.com>
2018-11-29 20:07:51 +0800

    .

--- sqlMap.js
+++ sqlMap.js
@@ -2,11 +2,11 @@ var sqlMap = {
     // getValue: 'SELECT * FROM content WHERE id = ?',
     setUpdate: 'UPDATE content SET title = ?,detailed= ?  WHERE id=?',
     // getContentAll: 'select * from content order by time desc'
-    getContentAll: 'SELECT * FROM content ORDER BY time DESC',
+    getContentAll: 'SELECT * FROM content WHERE userid = ? ORDER BY time DESC',
     getUsers: 'SELECT * FROM users',
     getValue: 'SELECT * FROM content WHERE id = ?',
-    getContentAllforKind: 'SELECT * FROM content WHERE kind=(SELECT kind FROM kinds WHERE id = ?) ORDER BY time DESC',
-    addValue: 'INSERT INTO content (title,detailed,time,kind,dateTim) VALUES (?,?,?,?,?)',
+    getContentAllforKind: 'SELECT * FROM content WHERE kind=(SELECT kind FROM kinds WHERE id = ?) AND userid = ? ORDER BY time DESC',
+    addValue: 'INSERT INTO content (title,detailed,time,kind,dateTim,userid) VALUES (?,?,?,?,?,?)',
     getKind: 'SELECT kind FROM kinds WHERE id=?',
     deleteValue: 'DELETE FROM content WHERE id=?'
   }
